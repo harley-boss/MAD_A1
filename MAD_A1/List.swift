6267bb358fc32efd7ae2da4c7debde43c72bd70b
Harley Boss <harleyaboss@gmail.com>
2019-09-26 08:53:02 -0400

    second commit

---
+++ MAD_A1/List.swift
@@ -0,0 +1,40 @@
+//
+//  ListItem.swift
+//  MAD_A1
+//
+//  Created by Harley Boss on 2019-09-24.
+//  Copyright © 2019 Saline Solutions. All rights reserved.
+//
+
+import Foundation
+
+// This class represents a list item entry
+
+class List {
+    
+    var id: Int = 0
+    var listItems = [String]()
+    
+    // Add an item to the list
+    public func addItem(_ itemName : String) {
+        listItems.append(itemName)
+    }
+    
+    // Removes a single occurance of an item from the list based on the name
+    // Case-sensitive
+    public func removeItem(_ itemName : String) {
+        for item in listItems {
+            if itemName == item {
+                if let index = listItems.firstIndex(of: item) {
+                    listItems.remove(at: index)
+                }
+                break
+            }
+        }
+    }
+    
+    // Get all items in the list
+    public func getAllItems() -> [String] {
+        return listItems
+    }
+}

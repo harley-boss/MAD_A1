f816ff37fac119e2969e069cec0221958574ff41
Student <student@Students-MacBook-Pro.local>
2019-10-01 15:14:08 -0400

    fixed button click on first tab that directs to second tab. Second tab now contains a textField that can take user input and on click of the overlayed button or enter pressed will add the text of the textfield to the textView

--- MAD_A1/EditList.swift
+++ MAD_A1/EditList.swift
@@ -8,15 +8,53 @@
 
 import UIKit
 
-class EditListController: UIViewController {
+class EditListController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var listNameLabel: UILabel!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var textField: UITextField!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
+        super.viewDidLoad() ;
+        setupUI();
+    }
+    
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        performAction()
+        return true
+    }
+    
+    @IBAction func onClick(){
+        performAction()
+    }
+    
+    func performAction() {
+         let newItemStr: String = textField.text!
+        
+        textView?.text += (newItemStr + "\n")
+        
+        textField.text = ""
     }
     
     func setList() {
+        
+    }
+    
+    func setupUI(){
+        self.textField.delegate = self
+        
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(named: "add.png"), for: .normal)
+        button.frame = CGRect(x: textField.frame.size.width - 40 , y: 5, width: 22, height: 22)
+        button.addTarget(self, action: #selector(self.onClick), for: .touchUpInside)
+        
+        textField.rightView = button
+        textField.rightViewMode = .always
         
     }
 }

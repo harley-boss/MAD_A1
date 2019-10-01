110e2eb62092b2f33f8a4f8f90e726bc7d6b253c
JSTRUK <10167488+JStruk@users.noreply.github.com>
2019-10-01 15:34:31 -0400

    Wip on second tab (#2)
    * fixed button click on first tab that directs to second tab. Second tab now contains a textField that can take user input and on click of the overlayed button or enter pressed will add the text of the textfield to the textView
    
    * Cleanup

--- MAD_A1/CreateList.swift
+++ MAD_A1/CreateList.swift
@@ -15,15 +15,21 @@ class CreateListController: UIViewController {
     @IBOutlet weak var create: UIButton!
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        create.addTarget(self, action: Selector(("buttonClicked:")), for: .touchDown)
     }
+    
+    @IBAction func createClicked(_ sender: Any) {
+        self.tabBarController?.selectedIndex = 1
+        
+        listName.text = "ok"
+    }
+    
     
     @IBAction func listNameChanged(_ sender: UITextField) {
         // Ensure that the string isn't empty any maybe at least 3 chars?
-        create.isEnabled = true
     }
     
     @IBAction func listCreate(_ sender: Any) {

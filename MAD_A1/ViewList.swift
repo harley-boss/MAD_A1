ba5bbe41def881175209df57e30994531159580f
Nathan Davis <31926099+NDavisDevelopment@users.noreply.github.com>
2019-10-01 21:42:40 -0400

    renders single dropdown list (#3)

--- MAD_A1/ViewList.swift
+++ MAD_A1/ViewList.swift
@@ -14,9 +14,50 @@ import UIKit
 
 class ViewListController: UIViewController {
     
+    @IBOutlet weak var tblView: UITableView!
+    @IBOutlet weak var GroceryList: UIButton!
+    @IBOutlet weak var btnDrop: UIButton!
+    
+    var fruitList = ["Orange","Banana","Strawberry","Cherry","Plumb","Apple","Mango"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        GroceryList.layer.cornerRadius=5
+        tblView.isHidden = true
         
     }
+    @IBAction func onClickDropButton(_ sender: Any) {
+        
+        if tblView.isHidden {
+            animate(toggle: true)
+        } else {
+            animate(toggle: false)
+        }
+    }
+    
+    func animate(toggle: Bool) {
+        if toggle {
+             UIView.animate(withDuration: 0.3, animations: {
+                       self.tblView.isHidden = false
+                   })
+        } else {
+             UIView.animate(withDuration: 0.3, animations: {
+                       self.tblView.isHidden = true
+                   })
+        }
+    }
+}
+
+extension ViewListController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruitList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = fruitList[indexPath.row]
+        return cell
+    }
+    
+
 }

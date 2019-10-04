c79b9ef56d325b36d63da8f582b5486369f64689
nathan <nathan.webdavis@gmail.com>
2019-10-04 08:49:59 -0400

    add comments

--- MAD_A1/ViewList.swift
+++ MAD_A1/ViewList.swift
@@ -1,28 +1,40 @@
-//
-//  ViewList.swift
-//  MAD_A1
-//
-//  Created by Harley Boss on 2019-09-27.
-//  Copyright © 2019 Saline Solutions. All rights reserved.
-//
-
-
-// Use this link for the View tab to create an expandable table
-// https://medium.com/ios-os-x-development/ios-how-to-build-a-table-view-with-collapsible-sections-96badf3387d0
-
+/*
+* FILE : ViewList.swift
+* PROJECT : PROG3230 - Assignment #1
+* PROGRAMMER : Nathan Davis
+* FIRST VERSION : 10-04-2019
+* DESCRIPTION :
+* Displays expandable grocery lists
+*/
 import UIKit
 
 class ViewListController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    let groceryList = [GroceryList(0, "Grocery List 1", ["Orange", "Purple", "Blue"]), GroceryList(1, "Grocery List 2", ["Rice", "Beans", "Carrots", "Harley"]), GroceryList(1, "Grocery List 3", ["Buns", "Burgers", "Hot Dogs", "Harley"]), GroceryList(1, "Grocery List 4", ["Nathan", "Justin", "Spencer", "Harley"]), ]
     
-    let lists = ["Grocery list 1", "Grocery list 2", "Grocery list 3"]
+    // Hard coded grocery lists for this iteration of the assignment
+    let groceryList = [GroceryList(0, "Grocery List 1", ["Orange", "Purple", "Blue"]), GroceryList(1, "Grocery List 2", ["Rice", "Beans", "Carrots", "Harley"]), GroceryList(1, "Grocery List 3", ["Buns", "Burgers", "Hot Dogs", "Harley"]), GroceryList(1, "Grocery List 4", ["Nathan", "Justin", "Spencer", "Harley"]), ]
     @IBOutlet weak var tableView: UITableView!
+
+    
+    /*
+    * Function : viewDidLoad
+    * Description : On create lifecycle hook
+    * Paramaters : None
+    * Returns : None
+    */
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.tableView.backgroundColor = .white
         self.tableView.tableFooterView = UIView()
     }
+    
+    /*
+    * Function : tableView(_ tableView: UITableView, viewForHeaderInSection section: Int)
+    * Description : Renders a button within the tableView headers
+    *               and adds hadnleOpenClose action
+    * Paramaters : None
+    * Returns : None
+    */
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         
         let button = UIButton(type: .system)
@@ -38,9 +50,24 @@ class ViewListController: UIViewController, UITableViewDataSource, UITableViewDe
         return button
     }
     
+    /*
+    * Function : tableView(_ tableView: UITableView, heightForHeaderInSection section: Int)
+    * Description : Sets header height
+    * Paramaters : None
+    * Returns : None
+    */
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    /*
+    * Function : handleOpenClose(button: UIButton)
+    * Description : Collapses and expands tableview sections
+    * Paramaters : button: UIButton
+    * Returns : None
+    */
     @objc func handleOpenClose(button: UIButton) {
         let section = button.tag
-        // we'll try to close the seciton first by deleting the rows
         var indexPaths = [IndexPath]()
         for row in groceryList[section].getAllItems().indices {
             let indexPath = IndexPath(row: row, section: section)
@@ -58,23 +85,48 @@ class ViewListController: UIViewController, UITableViewDataSource, UITableViewDe
         
 
     }
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 50
-    }
+    
+    /*
+    * Function : numberOfSections(in tableView: UITableView)
+    * Description : Sets number of sections in tableview
+    * Paramaters : None
+    * Returns : None
+    */
     func numberOfSections(in tableView: UITableView) -> Int {
         return groceryList.count
     }
+    
+    /*
+    * Function : tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath)
+    * Description : set cell background and text color
+    * Paramaters : None
+    * Returns : None
+    */
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         cell.backgroundColor = UIColor.clear
         cell.textLabel?.textColor = .black
 
     }
+    
+   /*
+   * Function : tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)
+   * Description : set number of rows in each list section
+   * Paramaters : None
+   * Returns : None
+   */
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if(!groceryList[section].isExpanded) {
             return 0
         } 
         return groceryList[section].getAllItems().count
     }
+    
+    /*
+    * Function : tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)
+    * Description : returns a single cell for a single row
+    * Paramaters : None
+    * Returns : None
+    */
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellList", for: indexPath)
         

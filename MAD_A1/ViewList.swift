af11a1fa38f86e028d85bd76255fac6d6a4390fe
nathan <nathan.webdavis@gmail.com>
2019-10-04 00:01:14 -0400

    update variable name

--- MAD_A1/ViewList.swift
+++ MAD_A1/ViewList.swift
@@ -13,7 +13,7 @@
 import UIKit
 
 class ViewListController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    let actualList = [GroceryList(0, "Grocery List 1", ["Orange", "Purple", "Blue"]), GroceryList(1, "Grocery List 2", ["Rice", "Beans", "Carrots", "Harley"]), GroceryList(1, "Grocery List 3", ["Buns", "Burgers", "Hot Dogs", "Harley"]), GroceryList(1, "Grocery List 4", ["Nathan", "Justin", "Spencer", "Harley"]), ]
+    let groceryList = [GroceryList(0, "Grocery List 1", ["Orange", "Purple", "Blue"]), GroceryList(1, "Grocery List 2", ["Rice", "Beans", "Carrots", "Harley"]), GroceryList(1, "Grocery List 3", ["Buns", "Burgers", "Hot Dogs", "Harley"]), GroceryList(1, "Grocery List 4", ["Nathan", "Justin", "Spencer", "Harley"]), ]
     
     let lists = ["Grocery list 1", "Grocery list 2", "Grocery list 3"]
     @IBOutlet weak var tableView: UITableView!
@@ -26,7 +26,7 @@ class ViewListController: UIViewController, UITableViewDataSource, UITableViewDe
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         
         let button = UIButton(type: .system)
-        button.setTitle(actualList[section].getListName(), for: .normal)
+        button.setTitle(groceryList[section].getListName(), for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.backgroundColor = .systemTeal
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
@@ -42,12 +42,12 @@ class ViewListController: UIViewController, UITableViewDataSource, UITableViewDe
         let section = button.tag
         // we'll try to close the seciton first by deleting the rows
         var indexPaths = [IndexPath]()
-        for row in actualList[section].getAllItems().indices {
+        for row in groceryList[section].getAllItems().indices {
             let indexPath = IndexPath(row: row, section: section)
             indexPaths.append(indexPath)
         }
-        let isExpanded = actualList[section].isExpanded
-        actualList[section].isExpanded = !isExpanded
+        let isExpanded = groceryList[section].isExpanded
+        groceryList[section].isExpanded = !isExpanded
         if(isExpanded) {
             tableView.deleteRows(at: indexPaths, with: .fade)
         }
@@ -62,7 +62,7 @@ class ViewListController: UIViewController, UITableViewDataSource, UITableViewDe
         return 50
     }
     func numberOfSections(in tableView: UITableView) -> Int {
-        return actualList.count
+        return groceryList.count
     }
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         cell.backgroundColor = UIColor.clear
@@ -70,15 +70,15 @@ class ViewListController: UIViewController, UITableViewDataSource, UITableViewDe
 
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if(!actualList[section].isExpanded) {
+        if(!groceryList[section].isExpanded) {
             return 0
         } 
-        return actualList[section].getAllItems().count
+        return groceryList[section].getAllItems().count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellList", for: indexPath)
         
-        let listName = actualList[indexPath.section].getAllItems()[indexPath.row]
+        let listName = groceryList[indexPath.section].getAllItems()[indexPath.row]
             cell.textLabel?.text = listName
         return cell
     }
